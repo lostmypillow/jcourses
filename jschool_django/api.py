ae8806d150c53aec9081cba49cd5770c8969a596
Johnny Lin <47119111+lostmypillow@users.noreply.github.com>
2024-05-06 03:30:57 +0800

    complete all GET methods (sort of)

--- jschool_django/api.py
+++ jschool_django/api.py
@@ -1,61 +1,65 @@
-import re
-import time
-
-import requests
+import re, requests
 from bs4 import BeautifulSoup
 from ninja import NinjaAPI
 
 api = NinjaAPI()
 
 
-@api.get("/hello")
-def hello(request):
-    return "Hello world"
-
-
-@api.get("/year")
-def get_year_and_semester(request):
-    start_time = time.time()
+@api.get("/sems")
+def get_semester(request):
     lst = []
     base_url = "https://aps.ntut.edu.tw/course/tw/courseSID.jsp"
     years = BeautifulSoup(requests.get(base_url).content, "html.parser")
     get_real_year = years.find_all(lambda tag: tag.name == 'a' and tag.get('href', '').startswith('Subj'))
     for g in get_real_year:
-        dep_list = []
-        pattern = r'\d+'
-        find_sem = re.findall(pattern, g.text)
-        basee = "https://aps.ntut.edu.tw/course/tw/" + g['href']
-        get_deps = BeautifulSoup(requests.get(basee).content, "html.parser").find_all("a")
-        for dep in get_deps:
-            year_list = []
-            start_index = dep['href'].find("code=")
-            code = dep['href'][start_index + len("code="):]
-            base4 = "https://aps.ntut.edu.tw/course/tw/" + dep['href']
-            get_year = BeautifulSoup(requests.get(base4).content, "html.parser").find_all("a")
-            for ye in get_year:
-                start_index = ye['href'].find("code=")
-                code = ye['href'][start_index + len("code="):]
-                year_list.append({
-                    "name": ye.text,
-                    "link": ye['href'],
-                    "code": code,
-                })
-            dep_list.append({
-                "name": dep.text,
-                "code": code,
-                "link": dep['href'],
-                "years": year_list,
-            })
+        extract_sem_year = re.findall(r'\d+', g.text)
+
         lst.append({
-            "year": find_sem[0],
-            "sem": find_sem[1],
-            "link": dep_list
+            "year": extract_sem_year[0],
+            "sem": extract_sem_year[1],
         })
-    end_time = time.time()
-    scraping_time = end_time - start_time
-    print(f"Scraping time: {scraping_time} seconds")
     return lst
 
+
+@api.get("/deps")
+def get_departments(request, year, sem):
+    lst = []
+
+    def scrape(url):
+        return BeautifulSoup(requests.get(url).content, "html.parser").find_all("a")
+
+    base_url = "https://aps.ntut.edu.tw/course/tw/Subj.jsp?format=-2&year=" + str(year) + "&sem=" + str(sem)
+    data = scrape(base_url)
+    for dep in data:
+        code = dep['href'][dep['href'].find("code=") + len("code=")]
+        lst.append({
+            "name": dep.text,
+            "link": dep['href'],
+            "code": code,
+        })
+    return lst
+
+
+@api.get("/years")
+def get_years(request, year, sem, code):
+    lst = []
+
+    def scrape(url):
+        return BeautifulSoup(requests.get(url).content, "html.parser").find_all("a")
+
+    base_url = "https://aps.ntut.edu.tw/course/tw/Subj.jsp?format=-3&year=" + str(year) + " &sem=" + str(
+        sem) + "&code=" + str(code)
+    # https://aps.ntut.edu.tw/course/tw/Subj.jsp?format=-3&year=112&sem=2&code=54
+    data = scrape(base_url)
+    for year in data:
+        lst.append({
+            "name": year.text,
+            "code": year['href'],
+        })
+    return lst
+
+
+# it doesn't return code
 
 @api.get("/course")
 def get_course(request):
@@ -75,69 +79,3 @@ def get_course(request):
 
     # Print the resulting list
     return {"text": grouped_text}
-
-
-
-@api.get("/year_alt")
-def get_it(request):
-    def combine(base):
-        return "https://aps.ntut.edu.tw/course/tw/" + str(base)
-
-    def scrape(url):
-        return BeautifulSoup(requests.get(url).content, "html.parser").find_all("a")
-
-    count = 0
-    start_time = time.time()
-    lst = []
-    scrape_dep_list = []
-    dep_list = [[] for _ in range(2)]
-    base_url = "https://aps.ntut.edu.tw/course/tw/courseSID.jsp"
-    years = BeautifulSoup(requests.get(base_url).content, "html.parser")
-    get_real_year = years.find_all(lambda tag: tag.name == 'a' and tag.get('href', '').startswith('Subj'))
-    for g in get_real_year:
-        extract_sem_year = re.findall(r'\d+', g.text)
-        basee = combine(g['href'])
-        scrape_dep_list.append(basee)
-        lst.append({
-            "year": extract_sem_year[0],
-            "sem": extract_sem_year[1],
-        })
-    scrape_year_list = [[] for _ in range(2)]
-
-    for link in scrape_dep_list:
-        dep_data = scrape(link)
-        # print(count)
-        for dep in dep_data:
-            code = dep['href'][dep['href'].find("code=") + len("code=")]
-            dep_list[count].append({
-                "name": dep.text,
-                "link": dep['href'],
-                "code": code,
-            })
-            scrape_year_list[count].append(combine(dep['href']))
-        count += 1
-    k = []
-    for idx, item in enumerate(lst):
-        item["list"] = dep_list[idx]
-
-    for s in scrape_year_list:
-
-        for d in s:
-
-            list_of_a = scrape(d)
-            n = []
-            for fs in list_of_a:
-                ll = {
-                    "name": fs.text,
-                    "link": fs['href'],
-                }
-                n.append(ll)
-
-            k.append(n)
-
-    for idx, item in enumerate(dep_list[0]):
-        item["years"] = k[idx]
-    end_time = time.time()
-    scraping_time = end_time - start_time
-    print(f"Scraping time: {scraping_time} seconds")
-    return lst
