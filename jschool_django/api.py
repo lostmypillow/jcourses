ad36b3cac1acd2276be0b080c522c23517b5795b
Johnny Lin <47119111+lostmypillow@users.noreply.github.com>
2024-05-06 00:18:16 +0800

    implement alternative scraping method

--- jschool_django/api.py
+++ jschool_django/api.py
@@ -75,3 +75,69 @@ def get_course(request):
 
     # Print the resulting list
     return {"text": grouped_text}
+
+
+
+@api.get("/year_alt")
+def get_it(request):
+    def combine(base):
+        return "https://aps.ntut.edu.tw/course/tw/" + str(base)
+
+    def scrape(url):
+        return BeautifulSoup(requests.get(url).content, "html.parser").find_all("a")
+
+    count = 0
+    start_time = time.time()
+    lst = []
+    scrape_dep_list = []
+    dep_list = [[] for _ in range(2)]
+    base_url = "https://aps.ntut.edu.tw/course/tw/courseSID.jsp"
+    years = BeautifulSoup(requests.get(base_url).content, "html.parser")
+    get_real_year = years.find_all(lambda tag: tag.name == 'a' and tag.get('href', '').startswith('Subj'))
+    for g in get_real_year:
+        extract_sem_year = re.findall(r'\d+', g.text)
+        basee = combine(g['href'])
+        scrape_dep_list.append(basee)
+        lst.append({
+            "year": extract_sem_year[0],
+            "sem": extract_sem_year[1],
+        })
+    scrape_year_list = [[] for _ in range(2)]
+
+    for link in scrape_dep_list:
+        dep_data = scrape(link)
+        # print(count)
+        for dep in dep_data:
+            code = dep['href'][dep['href'].find("code=") + len("code=")]
+            dep_list[count].append({
+                "name": dep.text,
+                "link": dep['href'],
+                "code": code,
+            })
+            scrape_year_list[count].append(combine(dep['href']))
+        count += 1
+    k = []
+    for idx, item in enumerate(lst):
+        item["list"] = dep_list[idx]
+
+    for s in scrape_year_list:
+
+        for d in s:
+
+            list_of_a = scrape(d)
+            n = []
+            for fs in list_of_a:
+                ll = {
+                    "name": fs.text,
+                    "link": fs['href'],
+                }
+                n.append(ll)
+
+            k.append(n)
+
+    for idx, item in enumerate(dep_list[0]):
+        item["years"] = k[idx]
+    end_time = time.time()
+    scraping_time = end_time - start_time
+    print(f"Scraping time: {scraping_time} seconds")
+    return lst
