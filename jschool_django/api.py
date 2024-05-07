8485174af25f84f534a9ec1246759de3932b78c2
Johnny Lin <47119111+lostmypillow@users.noreply.github.com>
2024-05-07 15:51:25 +0800

    improved readability of get sem deps and years

--- jschool_django/api.py
+++ jschool_django/api.py
@@ -4,78 +4,72 @@ from ninja import NinjaAPI
 
 api = NinjaAPI()
 
+base_url = "https://aps.ntut.edu.tw/course/tw/"
+
+
+def combine(url):
+    return base_url + str(url)
+
+
+def scrape(url, tag):
+    if tag == "td":
+        return BeautifulSoup(requests.get(url).content, "html.parser").find(str(tag))
+
+    elif tag != "":
+        return BeautifulSoup(requests.get(url).content, "html.parser").find_all(str(tag))
+    else:
+        return BeautifulSoup(requests.get(url).content, "html.parser").find_all(
+            lambda tag: tag.name == 'a' and tag.get('href', '').startswith('Subj'))
+
 
 @api.get("/sems")
 def get_semester(request):
-    lst = []
-    base_url = "https://aps.ntut.edu.tw/course/tw/courseSID.jsp"
-    years = BeautifulSoup(requests.get(base_url).content, "html.parser")
-    get_real_year = years.find_all(lambda tag: tag.name == 'a' and tag.get('href', '').startswith('Subj'))
-    for g in get_real_year:
-        extract_sem_year = re.findall(r'\d+', g.text)
-
-        lst.append({
+    sem_list = []
+    sems = scrape(combine("courseSID.jsp"), "")
+    for sem in sems:
+        extract_sem_year = re.findall(r'\d+', sem.text)
+        sem_list.append({
             "year": extract_sem_year[0],
             "sem": extract_sem_year[1],
+            "link": sem['href'],
         })
-    return lst
+    print(time)
+    return sem_list
 
 
 @api.get("/deps")
-def get_departments(request, year, sem):
-    lst = []
-
-    def scrape(url):
-        return BeautifulSoup(requests.get(url).content, "html.parser").find_all("a")
-
-    base_url = "https://aps.ntut.edu.tw/course/tw/Subj.jsp?format=-2&year=" + str(year) + "&sem=" + str(sem)
-    data = scrape(base_url)
-    for dep in data:
-        code = dep['href'][dep['href'].find("code=") + len("code=")]
-        lst.append({
+def get_departments(request, link):
+    dep_list = []
+    deps = scrape(combine(link))
+    for dep in deps:
+        dep_list.append({
             "name": dep.text,
             "link": dep['href'],
-            "code": code,
         })
-    return lst
+    return dep_list
 
 
 @api.get("/years")
-def get_years(request, year, sem, code):
-    lst = []
-
-    def scrape(url):
-        return BeautifulSoup(requests.get(url).content, "html.parser").find_all("a")
-
-    base_url = "https://aps.ntut.edu.tw/course/tw/Subj.jsp?format=-3&year=" + str(year) + " &sem=" + str(
-        sem) + "&code=" + str(code)
+def get_years(request, link):
+    year_list = []
     # https://aps.ntut.edu.tw/course/tw/Subj.jsp?format=-3&year=112&sem=2&code=54
-    data = scrape(base_url)
-    for year in data:
-        lst.append({
+    years = scrape(combine(link))
+    for year in years:
+        year_list.append({
             "name": year.text,
-            "code": year['href'],
+            "link": year['href'],
         })
-    return lst
+    return year_list
 
-
-# it doesn't return code
 
 @api.get("/course")
 def get_course(request):
     lst = []
-    base_url = "https://aps.ntut.edu.tw/course/tw/Subj.jsp?format=-4&year=112&sem=2&code=2683"
-    course_data = BeautifulSoup(requests.get(base_url).content, "html.parser").find('td')
-    x = course_data.text
-    y = x.replace(" ", "").replace("　", "")
-    text_list = y.split("\n")
-
-    # Remove empty strings from the list
-    items = [line.strip() for line in text_list if line.strip()]
-    matches = [i for i, item in enumerate(items) if re.match(r'\b\d{6}\b', item)]
-
-    # Group the text between each sequence of six numbers
-    grouped_text = [items[matches[i]:matches[i + 1]] for i in range(len(matches) - 1)] + [items[matches[-1]:]]
-
-    # Print the resulting list
+    uurl = "Subj.jsp?format=-4&year=112&sem=2&code=2683"
+    course_data = scrape(combine(uurl), "td")
+    x = course_data.text.replace(" ", "").replace("　", "").split("\n")
+    stripx = [line.strip() for line in x if line.strip()]
+    matches = [i for i, item in enumerate(stripx) if re.match(r'\b\d{6}\b', item)]
+    grouped_text = [stripx[matches[i]:matches[i + 1]] for i in range(len(matches) - 1)] + [stripx[matches[-1]:]]
     return {"text": grouped_text}
+
