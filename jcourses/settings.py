b76903ff1b37e21b07bb0128f817b2371be18f63
Johnny <47119111+lostmypillow@users.noreply.github.com>
2024-08-08 13:04:31 +0800

    corrected erroneous docker image name

--- jcourses/settings.py
+++ jcourses/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['jcourses.lostmypillow.duckdns.org', 'localhost', '127.0.0.1', 'jcourses.onrender.com']
+ALLOWED_HOSTS = ['jcourses.lostmypillow.duckdns.org', 'localhost', '127.0.0.1', '0.0.0.0', 'jcourses.onrender.com']
 
 
 # Application definition
